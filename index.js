0b67f43c3487091f9286449602265b52d5058077
Fernando Donoso <fidonoso@gmail.com>
2022-03-17 22:30:01 -0300

    Desafio Abracadabra

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const fs=require('fs')
+
+
 const app = express()
-
-
 app.listen(3000, () => {
-    console.log('El servidor está inicializado en el puerto 3000')
+    console.log('El servidor está escuchando en el puerto 3000')
 });
 
 app.use(express.static("assets"));
@@ -13,7 +13,14 @@ app.use('/abracadabra/juego/:usuario', (req, res, next)=>{
     let usuario = req.params.usuario
     let usuarios = JSON.parse(fs.readFileSync(__dirname +'/db/usuarios.json', 'utf8'))
     let respuesta= usuarios.usuarios.includes(usuario)
-    respuesta? next(): res.send(`No existe el usuario ${usuario}`)
+    respuesta? next(): res.sendFile(__dirname +'/assets/who.jpeg')
+});
+
+app.use('/abracadabra/conejo/:n', (req, res, next)=>{
+    let n = Number(req.params.n)
+    let nAzar= Math.floor(Math.random()*(4-1))+1
+    console.log(n, nAzar)
+    n==nAzar? next(): res.sendFile(__dirname +'/assets/voldemort.jpg')
 });
 
 
@@ -23,13 +30,15 @@ app.get('/', (req, res)=>{
 
 app.get("/abracadabra/usuarios", (req, res) => {
     res.sendFile(__dirname+'/db/usuarios.json')
-    });
+});
 
+app.get('/abracadabra/juego/:usuario', (req, res, next)=>{
+    res.send(`<h1>Usuario ${req.params.usuario} si existe en el servidor </h1>`)
+});
 
-
-
-
-
+app.get('/abracadabra/conejo/:n',(req, res)=>{
+    res.sendFile(__dirname +'/assets/conejito.jpg')
+})
 
 
 app.get("*", (req, res) => {
